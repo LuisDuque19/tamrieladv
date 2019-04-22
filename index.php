e96160c6335cb14a35ac5d76f935cd31050b40de
LuisDuque <48967164+LuisDuque19@users.noreply.github.com>
2019-04-22 17:15:11 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Teste</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="main.css">
+</head>
+<body>
+	<center>
+	<header>
+		<div class="main-menu">
+			<input type="button" name="btn1" value="Home" onclick="" class="btn_menu">
+			<input type="button" name="btn2" value="Curiosidades" onclick="" class="btn_menu">
+			<input type="button" name="btn3" value="Exemplo3" onclick="" class="btn_menu">
+			<input type="button" name="btn4" value="Exemplo4" onclick="" class="btn_menu">
+			<input type="button" name="btn5" value="Exemplo5" onclick="" class="btn_menu">
+			<input type="button" name="btn6" value="Exemplo6" onclick="" class="btn_menu">
+		</div>
+	</header>
+
+	<div class="conteudo"></div>
+	</center>
+</body>
+</html>
