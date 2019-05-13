314dba0b6c2da36ba558a3e632fdc7eec301516f
Gabriel Alves <42902246+gabrielalves96@users.noreply.github.com>
2019-05-13 15:09:09 -0300

    login

---
+++ auth.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <title>Login</title>
+</head>
+<body>
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12" style="background-color: black; height: 92px;">
+                <img src="imgs/logo.png" style="float:left; width: 15%;">
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-12" style="background-image: url(imgs/tesbg.jpg); height:700px; background-repeat: no-repeat; background-size: 100% 100%;">
+                        <div style="align-content: center;">
+                                <form method="POST" action="#">
+                                Username:<br>
+                                <input type="text" name="username" value="">
+                                <br>
+                                Password:<br>
+                                <input type="text" name="pass" value="">
+                                <br><br>
+                                <input type="submit" value="Submit">
+                                </form>
+                        </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
