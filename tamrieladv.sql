526d38e0f448f4d9f72274b8bdd4c49965f5eaf6
Gabriel Alves <42902246+gabrielalves96@users.noreply.github.com>
2019-06-04 15:31:15 -0300

    Update tamrieladv.sql

--- tamrieladv.sql
+++ tamrieladv.sql
@@ -1,6 +1,8 @@
 
 create database tamrielnoticias character set UTF8mb4 collate utf8mb4_bin;
 
+USE tamrielnoticias;
+
 CREATE TABLE noticias (
     id int NOT NULL AUTO_INCREMENT,
     titulo varchar(100),
