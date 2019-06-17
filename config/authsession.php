bebb5642ea1ad6ff362c7b9887fc094abdae1d77
leon-nojima <37312229+leon-nojima@users.noreply.github.com>
2019-06-17 10:32:02 -0300

    Add files via upload

--- config/authsession.php
+++ config/authsession.php
@@ -1,30 +1,29 @@
 <?php
     
     session_start();
-    
-    $user = array("gabriel", "teste1", "teste2", "admin");
-    $pass = array("teste", "senha1", "senha2", "admin");
-    
-    $userlength = count($user);
+    include("./dbConnection.php");
 
-    $msg = FALSE;
-    
-    for($i = 0 ; $i < $userlength ; $i++) {
-        if($_POST["user"] == $user[$i] && $_POST["pass"] == $pass[$i]){
-            
-            $msg = TRUE;
-            break;
-            
-        }
+    if(empty($_POST['login']) || empty($_POST['senha'])){
+        header('Location: ../auth.php');
+        exit();
     }
 
-    if($msg){
-        
-        $_SESSION["logado"] = TRUE;
-        $_SESSION["user"] = $_POST["user"];
-        
-        header("Location: ../index.php");
-    }
-    else { header("Location: ../auth.php"); }
+    $login = mysqli_real_escape_string($connection, $_POST['login']);
+    $senha = mysqli_real_escape_string($connection, $_POST['senha']);
 
+    $query = "select Login from usuario where Login = '{$login}' and Senha = md5('{$senha}')";
+
+    $result = mysqli_query($connection, $query);
+
+    $row = mysqli_num_rows($result);
+
+    if($row == 1){
+        $_SESSION['login'] = $login;
+        $_SESSION['logado'] = true;
+        header('Location: ../index.php');
+    } else {
+        $_SESSION['nao_autenticado'] = true;
+        header('Location: ../auth.php');
+        exit();
+    }
 ?>
